2ef7abd61b77f3399d11da795c5270631718f4f4
AroaGO <74042599+aroago@users.noreply.github.com>
2020-11-16 08:34:34 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/ejercicioedd0/HolaMundo.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejercicioedd0;
+
+/**
+ * This class is hallowing me.
+ * @author daw1
+ */
+public class HolaMundo {
+
+    public static void main(String[] args){
+    
+        System.out.println("hola Mundo");
+    
+    }
+}
